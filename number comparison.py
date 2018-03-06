43751aec949226d470f5f74da4fd0760b0f76ebf
kameronjm <kameronjmyers@gmail.com>
2018-03-06 12:43:31 -0500

    initial upload

---
+++ number comparison.py
@@ -0,0 +1,6 @@
+number1 = raw_input('Enter first number: ')
+number2 = raw_input('Enter second number: ')
+number3 = raw_input('Enter third number: ')
+maximum = max(number1, number2, number3)
+
+print maximum
